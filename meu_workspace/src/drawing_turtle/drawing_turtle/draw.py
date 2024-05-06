88b18b0331ed02dd0a411f758c307185de01eca8
ItsVasconcellos <fernando.asv@uol.com.br>
2024-05-05 22:15:38 -0300

    feat: Adding setPen

--- meu_workspace/src/drawing_turtle/drawing_turtle/draw.py
+++ meu_workspace/src/drawing_turtle/drawing_turtle/draw.py
@@ -15,16 +15,21 @@ class DrawingTurtle(Node):
     def spawn(self):
         spawn = self.spawn_turtle.call_async(Spawn.Request(x=5.5, y=5.5, theta=0.0, name=self.turtle_name))
         rclpy.spin_until_future_complete(self, spawn)
+        new_turtle_name = spawn.result().name
+        print(new_turtle_name)
 
     def kill(self):
         kill = self.kill_turtle.call_async(Kill.Request(name=self.turtle_name))
         rclpy.spin_until_future_complete(self, kill)
 
+    def set_pen(self):
+        set_pen = self.set_pen.call_async(SetPen.Request(r=255, g=255, b=255, width=1, off=0))
+        rclpy.spin_until_future_complete(self, set_pen)
+
 def main(args=None):
     rclpy.init(args=args)
     draw_turtle = DrawingTurtle("VASC0")
-    draw_turtle.spawn() 
-    print("Teste")
+    draw_turtle.spawn()
 
 if __name__ == '__main__':
     main()
